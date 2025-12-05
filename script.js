e82664c7810ad7bc3578d583d2cb4277cc6f63e9
gayaa370 <gayaa370@gmail.com>
2025-12-05 14:33:10 +0200

    update

--- script.js
+++ script.js
@@ -176,18 +176,4 @@
         };
     };
 
-
-    window.utils.getCookie = (cName) => {
-        const name = cName + "=";
-        const cDecoded = decodeURIComponent(document.cookie);
-        const cArr = cDecoded.split('; ');
-        let res;
-
-        cArr.forEach(val => {
-            if (val.indexOf(name) === 0) res = val.substring(name.length);
-        });
-
-        return res;
-    };
-
 })();
